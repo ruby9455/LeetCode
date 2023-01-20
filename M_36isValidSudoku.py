9e7a7b73fa803f3881bac11bc06b51dcd0c88968
ruby9455 <64198767+ruby9455@users.noreply.github.com>
2023-01-21 08:07:48 +1030

    Create M_36isValidSudoku.py

---
+++ M_36isValidSudoku.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # hold existing numbers in 
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        subgrids = defaultdict(set)
+        
+        # scan the board by row and col
+        for x in range(9):
+            for y in range(9):
+                # empty cell
+                if board[x][y] == ".":
+                    continue
+                # duplicate num
+                if board[x][y] in rows[x] or board[x][y] in cols[y] or board[x][y] in subgrids[(x/3),(y/3)]:
+                    return False
+                # new num
+                cols[y].add(board[x][y])
+                rows[x].add(board[x][y])
+                subgrids[(x/3),(y/3)].add(board[x][y])
+        return True
