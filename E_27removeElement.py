888593f619c72f0f53e9b5cc1b55ff2b39cb745f
ruby9455 <64198767+ruby9455@users.noreply.github.com>
2023-01-21 00:46:30 +1030

    Update E_27removeElement.py

--- E_27removeElement.py
+++ E_27removeElement.py
@@ -1,5 +1,7 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        # count no. of target value in the list
         count = nums.count(val)
+        # remove it for (count) times
         for i in range(count):
             nums.remove(val)
